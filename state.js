e6679d2680b50b0f3256c2db0d2f91fd161a110c
seasons986 <annalin12345@gmail.com>
2023-12-02 01:06:07 -0500

    really not all here

--- state.js
+++ state.js
@@ -2,9 +2,9 @@ window.addEventListener("load", () => {
   let container = document.getElementById("container");
   const states = container.dataset.states.split(",");
   let inc = +(container.dataset.state);
-  inc+=1;
   console.log((container.dataset.state),{states,inc});
   setInterval(() => {
+    inc+=1;
     document.getElementById('state').innerText = states[inc%states.length];
   }, 500);
 });
