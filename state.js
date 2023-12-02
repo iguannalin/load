348b731cf2244aaad87767bb742a2c7283e833e8
seasons986 <annalin12345@gmail.com>
2023-12-02 00:58:36 -0500

    halfawake

--- state.js
+++ state.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
   let container = document.getElementById("container");
-  const states = +(container.dataset.states);
+  const states = container.dataset.states;
   let inc = +(container.dataset.state);
   inc+=1;
   console.log((container.dataset.state),{states,inc});
