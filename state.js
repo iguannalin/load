1fbb6799b90ab4a23e163048d42351fa24a721f9
seasons986 <annalin12345@gmail.com>
2023-12-02 00:54:55 -0500

    oops

--- state.js
+++ state.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", () => {
   let container = document.getElementById("container");
-  let inc = +(container.dataset.state);
-  console.log((container.dataset.state),{states,inc});
-  inc+=1;
   const states = +(container.dataset.states);
+  let inc = +(container.dataset.state);
+  inc+=1;
+  console.log((container.dataset.state),{states,inc});
   setInterval(() => {
     document.getElementById('state').innerText = states[inc%states.length];
   }, 500);
